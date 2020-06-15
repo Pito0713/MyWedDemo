080c8e3d0f8ff555960aa6ba9f05e5e513cebdf6
Pito0713 <xli30334@gmail.com>
2020-06-16 00:54:16 +0800

    product fetch josn

--- JS/Product.js
+++ JS/Product.js
@@ -5,18 +5,6 @@ var Succulents = document.getElementById("Succulents")
 var Cultivation = document.getElementById("Cultivation")
 var Cactus = document.getElementById("Cactus")
 
-Catch = function (items){
-    for (let item of ProdcutItemPoint) {
-        if(item.id == items){
-            document.getElementById(item.id).style.display="block"
-        } else {
-           var CatchIt = document.getElementById(item.id)
-           CatchIt.style.display="none"
-           console.log(CatchIt)
-           
-        }
-     }
-}
 
 
 const Prodcut = "https://pito0713.github.io/Fetch/Product.json";
@@ -26,28 +14,148 @@ fetch(Prodcut)
     console.log(response)
     return response.json();
     //return response.text()
-}).then((myJson) => {
-    console.log(myJson);
-    let product;
-    for(let i = 0; i<myJson.length;i++){
-        product = myJson[i];
-        var content = product.content;
-        var id = product.id;
-        var img = product.img;
-        var name = product.name ;
-        var price = product.price;
-        document.getElementById("prodcutinfo").innerHTML  += "<div class='prodcutItem'>"+
-        "<div class='prodcutItemPoint' + id="+ id +">"+
-        "<img src="+ img +">"+
-        "<div class='prodcutItemName'>"+
-        "<a href='#'>" +name+"</a>"+
-        "<a class='prodcutItemPrice'>$"+ price + "</a>"+
-        "</div>"+
-        "<div class='prodcutItemContent'>"+
-        "<p>"+content+"</p>"+
-        "</div>"+
-        "</div>"+
-        "</div>"
-    }
-    
 })
+.then((myJson) => {
+    console.log(myJson)
+    catchItem(myJson);
+});
+
+
+
+function catchItem(myJson){
+    a = Array.from(myJson);
+    product = a[0];
+        document.getElementById("prodcutinfo-header").innerHTML  += "<div class='prodcutItem'>"+
+            "<div class='prodcutItemPoint' + id="+ product.id +">"+
+            "<img src="+ product.img +">"+
+            "<div class='prodcutItemName-Primary'>"+
+            "<a href='#'>" + product.name+"</a>"+
+            "<a class='prodcutItemPrice-Primary'>$"+ product.price+ "</a>"+
+            "</div>"+
+            "<div class='prodcutItemContent-Primary'>"+
+            "<p>"+ product.content+"</p>"+
+            "</div>"+
+            "</div>"+
+            "</div>"
+        for(let i = 1; i<3;i++){
+            product = a[i];
+            var content = product.content;
+            var id = product.id;
+            var img = product.img;
+            var name = product.name ;
+            var price = product.price;
+            document.getElementById("prodcutinfo-sider").innerHTML  += "<div class='prodcutItem'>"+
+            "<div class='prodcutItemPoint' + id="+ id +">"+
+            "<img src="+ img +">"+
+            "<div class='prodcutItemName'>"+
+            "<a href='#'>" +name+"</a>"+
+            "<a class='prodcutItemPrice'>$"+ price + "</a>"+
+            "</div>"+
+            "<div class='prodcutItemContent'>"+
+            "<p>"+content+"</p>"+
+            "</div>"+
+            "</div>"+
+            "</div>"
+            }    
+        for(let i = 3; i<a.length;i++){
+            product = a[i];
+            var content = product.content;
+            var id = product.id;
+            var img = product.img;
+            var name = product.name ;
+            var price = product.price;
+            document.getElementById("productinfo").innerHTML  += "<div class='prodcutItem'>"+
+            "<div class='prodcutItemPoint' + id="+ id +">"+
+            "<img src="+ img +">"+
+            "<div class='prodcutItemName'>"+
+            "<a href='#'>" +name+"</a>"+
+            "<a class='prodcutItemPrice'>$"+ price + "</a>"+
+            "</div>"+
+            "<div class='prodcutItemContent'>"+
+            "<p>"+content+"</p>"+
+            "</div>"+
+            "</div>"+
+            "</div>"
+        }
+}
+
+Catch = function (items){
+    console.log(a);
+    var data = a.filter(function(item){ 
+        return item.id == items;
+    });
+    console.log(data);
+    document.getElementById("prodcutinfo-header").innerHTML = "";
+    document.getElementById("prodcutinfo-sider").innerHTML = "";
+    document.getElementById("productinfo").innerHTML = "";
+        product = data[0];
+        document.getElementById("prodcutinfo-header").innerHTML  += "<div class='prodcutItem'>"+
+            "<div class='prodcutItemPoint' + id="+ product.id +">"+
+            "<img src="+ product.img +">"+
+            "<div class='prodcutItemName-Primary'>"+
+            "<a href='#'>" + product.name+"</a>"+
+            "<a class='prodcutItemPrice-Primary'>$"+ product.price+ "</a>"+
+            "</div>"+
+            "<div class='prodcutItemContent-Primary'>"+
+            "<p>"+ product.content+"</p>"+
+            "</div>"+
+            "</div>"+
+            "</div>"
+        for(let i = 1; i<3;i++){
+            product = data[i];
+            var content = product.content;
+            var id = product.id;
+            var img = product.img;
+            var name = product.name ;
+            var price = product.price;
+            document.getElementById("prodcutinfo-sider").innerHTML  += "<div class='prodcutItem'>"+
+            "<div class='prodcutItemPoint' + id="+ id +">"+
+            "<img src="+ img +">"+
+            "<div class='prodcutItemName'>"+
+            "<a href='#'>" +name+"</a>"+
+            "<a class='prodcutItemPrice'>$"+ price + "</a>"+
+            "</div>"+
+            "<div class='prodcutItemContent'>"+
+            "<p>"+content+"</p>"+
+            "</div>"+
+            "</div>"+
+            "</div>"
+            }    
+        for(let i = 3; i<data.length;i++){
+            product = data[i];
+            var content = product.content;
+            var id = product.id;
+            var img = product.img;
+            var name = product.name ;
+            var price = product.price;
+            document.getElementById("productinfo").innerHTML  += "<div class='prodcutItem'>"+
+            "<div class='prodcutItemPoint' + id="+ id +">"+
+            "<img src="+ img +">"+
+            "<div class='prodcutItemName'>"+
+            "<a href='#'>" +name+"</a>"+
+            "<a class='prodcutItemPrice'>$"+ price + "</a>"+
+            "</div>"+
+            "<div class='prodcutItemContent'>"+
+            "<p>"+content+"</p>"+
+            "</div>"+
+            "</div>"+
+            "</div>"
+        }
+    
+}
+
+
+                
+//<div class="prodcutItem">
+//<div class="prodcutItemPoint" id="Cactus">
+    //<img src="./img/about-5.jpeg" alt="about-2">
+    //<div class="prodcutItemName">
+        //<a href="#">仙人掌</a>
+        //<a class="prodcutItemPrice">$ 123</a>
+    //</div>
+    //<div class="prodcutItemContent">
+        //<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. .</p>
+    //</div>
+//</div>
+//</div>
+
