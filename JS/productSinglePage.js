f919c1a9263f8d3199ebf3f9ca8716466140949d
Pito0713 <xli30334@gmail.com>
2020-07-28 01:36:47 +0800

    Product rowData = sheetrow+2

--- JS/productSinglePage.js
+++ JS/productSinglePage.js
@@ -90,7 +90,7 @@ Cart = function(name){
     url: 'https://docs.google.com/spreadsheets/d/1XhsYtznJn5Ps3kF2ZXS0pAAuxy3SBksRGty7pMaL0GA/edit#gid=0',
     name: '工作表1',
     data: data.toString(),
-    row: number+1,
+    row: number+2,
     column:data[0].length
   };
   $.get('https://script.google.com/macros/s/AKfycbwrmkkL4oSV_qgbVV1byYPJYoKnRbq7vstu6-urmTVlTDbv2ZU/exec', parameter, function (data) {
